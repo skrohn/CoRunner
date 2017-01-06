04d5668d8546c3f2385f5b99a06f1c9e1a3d53aa
skrohn <krohn.stephan@googlemail.com>
2017-01-06 22:15:21 +0100

    allow yielding on other ienumerators
    + return immediately if all other enumerators also return immediately

--- Assets/CoRunner.cs
+++ Assets/CoRunner.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class WooRoutine
+{
+
+}
+
 public static class CoRunner
 {
-	private static readonly List<IEnumerator> routines = new List<IEnumerator>();
+	private static readonly HashSet<IEnumerator> routines = new HashSet<IEnumerator>();
 	private static readonly List<IEnumerator> routinesToAdd = new List<IEnumerator>();
+
 	private static UniProxy proxy;
 	private static bool isInitialized;
 
@@ -23,28 +29,43 @@ public static class CoRunner
 	{
 		if (!isInitialized) Init();
 
-		routinesToAdd.Add(routine);
-		routine.MoveNext();
+		bool hasNext;
+		do {
+			hasNext = routine.MoveNext();
+		} while (hasNext && routine.Current is IEnumerator && IsFinished((IEnumerator)routine.Current));
+
+		if (hasNext) {
+			routinesToAdd.Add(routine);
+		}
 		return routine;
 	}
 
 	public static void Update ()
 	{
-		for (var i = 0; i < routines.Count; i++) {
-			Debug.Log("update: " + routines[i].Current);
-			if (ShouldCall(routines[i].Current)) {
-				var keepRunning = routines[i].MoveNext();
-				if (!keepRunning) {
-					routines.RemoveAt(i--);
-				}
-			}
+		routines.RemoveWhere(Process);
+	}
+
+	static bool Process (IEnumerator item) {
+//		Debug.Log("update current: " + item.Current + " count: " + routines.Count);
+		if (ShouldCall(item.Current)) {
+			var result = item.MoveNext();
+			Debug.Log(!result);
+			return !result;
 		}
+		return false;
 	}
 
 	public static void LateUpdate ()
 	{
-		routines.AddRange(routinesToAdd);
+		foreach (var r in routinesToAdd) {
+			routines.Add(r);
+		}
 		routinesToAdd.Clear();
+	}
+
+	static bool IsFinished (IEnumerator enumerator)
+	{
+		return !routines.Contains(enumerator) && !routinesToAdd.Contains(enumerator);
 	}
 
 	private static bool ShouldCall (object obj)
@@ -59,7 +80,7 @@ public static class CoRunner
 		}
 
 		if (obj is IEnumerator) {
-//			((IEnumerator)obj).
+			return IsFinished((IEnumerator) obj);
 		}
 
 		Debug.LogWarning("Didn't know what to do with " + obj.GetType());
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,27 +7,57 @@ public class Test : MonoBehaviour {
 	void Start () {
 //		CoRunner.Start(coBasic());
 //		CoRunner.Start(coWWW());
-		CoRunner.Start(coIEnumerator());
+//		CoRunner.Start(coIEnumerator());
+//		CoRunner.Start(coRunImmediate());
+//		CoRunner.Start(coMultiYield());
 	}
 
 	IEnumerator coBasic ()
 	{
 		Debug.Log(Time.frameCount);
 		yield return null;
-		Debug.Log(Time.frameCount);
+		Debug.Log("coBasic finished " + Time.frameCount);
 	}
 
 	IEnumerator coWWW ()
 	{
-		Debug.Log(Time.frameCount);
+		Debug.Log("start www " + Time.frameCount);
 		yield return new WWW("http://www.google.com");
-		Debug.Log(Time.frameCount);
+		Debug.Log("coWWW finished " + Time.frameCount);
 	}
 
 	IEnumerator coIEnumerator ()
 	{
 		Debug.Log(Time.frameCount);
 		yield return CoRunner.Start(coBasic());
+		Debug.Log("coIEnumerator finished " + Time.frameCount);
+	}
+
+	IEnumerator coRunImmediate ()
+	{
 		Debug.Log(Time.frameCount);
+		yield return CoRunner.Start(coImmediate());
+		Debug.Log("coRunImmediate finished " + Time.frameCount);
+	}
+
+	IEnumerator coImmediate ()
+	{
+		if (Time.deltaTime < 0) {
+			yield return null;
+		}
+		Debug.Log("coImmediate finished " + Time.frameCount);
+	}
+
+	IEnumerator coYieldOn (IEnumerator enumerator)
+	{
+		yield return enumerator;
+		Debug.Log("finished YieldOn " + Time.frameCount);
+	}
+	IEnumerator coMultiYield ()
+	{
+		var routine = CoRunner.Start(coWWW());
+		CoRunner.Start(coYieldOn(routine));
+		yield return CoRunner.Start(coYieldOn(routine));
+		Debug.Log("finished MultiYield " + Time.frameCount);
 	}
 }
